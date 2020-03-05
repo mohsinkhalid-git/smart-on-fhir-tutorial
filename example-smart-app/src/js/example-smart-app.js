6b3d5e21ea3c3de1fdcfdbd67f7886406cf44cae
mohsinkhalid-git <60975611+mohsinkhalid-git@users.noreply.github.com>
2020-03-05 10:54:29 -0600

    Patient ICD and observations

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,6 +11,52 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
+/////////MK
+/*
+		var cond = smart.patient.api.fetchAll({
+                    type: 'Condition',
+                    query: {
+                      code: {
+                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      }
+                    }
+                  });
+				  
+        $.when(pt, cond).fail(onError);
+
+        $.when(pt, cond).done(function(patient, cond) {
+          var byCodes = smart.byCodes(cond, 'code');
+          
+          var icd = byCodes('8302-2');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var hdl = byCodes('2085-9');
+          var ldl = byCodes('2089-1');
+
+          var p = defaultPatient();
+          p.icd = patient.icd;
+          p.gender = gender;
+          p.fname = fname;
+          p.lname = lname;
+          p.height = getQuantityValueAndUnit(height[0]);
+
+          if (typeof systolicbp != 'undefined')  {
+            p.systolicbp = systolicbp;
+          }
+
+          if (typeof diastolicbp != 'undefined') {
+            p.diastolicbp = diastolicbp;
+          }
+
+          p.hdl = getQuantityValueAndUnit(hdl[0]);
+          p.ldl = getQuantityValueAndUnit(ldl[0]);
+
+          ret.resolve(p);
+        });
+*/
+////////MK
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
@@ -48,6 +94,9 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+		  //MK
+		  p.icd = patient.icd;
+		  //MK
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -83,6 +132,9 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+	  ///MK
+	  icd: {value: ''},
+	  ///MK
     };
   }
 
@@ -126,6 +178,9 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+	//MK
+	$('#icd').html(p.icd);
+	//MK
   };
 
 })(window);
